3e9e7fa953b5695ca4e990b1c065ba3776da8bba
Brady Kieffer <bradykieffer@gmail.com>
2017-03-24 17:20:49 -0400

    Can now PWM stuff

--- syde362_pid.ino
+++ syde362_pid.ino
@@ -50,6 +50,25 @@ void setup() {
 
 void loop() { 
   int delay_ms = 2500;
+  int ledPin = FET_ONE;
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  /*
+  
   Serial.println("FET_ONE IS HIGH AF");
   digitalWrite(FET_ONE, HIGH);
   delay(delay_ms);
@@ -57,7 +76,7 @@ void loop() {
   Serial.println("FET_ONE IS LOWWWWW");
   //Serial.print("FET_ONE: Proximity to plate: \t"); Serial.print(pollProximity()); Serial.print("\t\t\t\r");
   delay(delay_ms);
-
+  */
   /*
   Serial.println("FET_TWO IS HIGH AF");
   digitalWrite(FET_TWO, HIGH);
